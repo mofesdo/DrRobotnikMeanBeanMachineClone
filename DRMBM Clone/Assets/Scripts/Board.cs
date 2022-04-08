18ffa55ae926b327ac4fe816f65ac50ce51be11d
Modesto Amador <mofesdo.dev@gmail.com>
2022-04-07 20:26:36 -0700

    spawing and setting done
    pieces can now spawn and are set to the top of the board

--- DRMBM Clone/Assets/Scripts/Board.cs
+++ DRMBM Clone/Assets/Scripts/Board.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+        for(int i = 0; i < this.tetrominos.Length; i++)
+        {
+            this.tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominos.Length);
+        TetrominoData data = this.tetrominos[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 }
